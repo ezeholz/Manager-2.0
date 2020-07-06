edeb1c67d652957fa3b1b8604b3cb4de490d941c
Glitch (hello-express) <none>
2020-07-06 20:07:02 +0000

    🐿🍩 Checkpoint ./commands/functions/rooms/commands/create.js:10282369/2202

---
+++ commands/functions/rooms/commands/create.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  
+  trigger: "create",
+  enabled: true,
+  
+  category: "Room",
+  description: "Create rooms",
+  
+  execute(Manager, author) {
+    let db = Manager.database;
+
+//     if(args[2]) { // Si tiene para cambiarlo
+//       switch(args[1]){
+//         case 'look': db.set('channelLook', args[2]).write(); break;
+//         case 'voice': db.set('voiceCategory', args[2]).write(); break;
+//         case 'text': db.set('textCategory', args[2]).write(); break;
+//         default: break;
+//       }
+//     }
+
+//     const values = db.getState()
+
+//     const embed = new MessageEmbed()
+//       .setTitle('Valores actuales')
+//       .addFields(
+//         {name: 'Channel to lookout', value: values.channelLook},
+//         {name: 'Category for voice', value: values.voiceCategory},
+//         {name: 'Category for text', value: values.textCategory},
+//       )
+//       .setColor('#dada3d')
+
+//     msg.channel.send(embed)
+    
+    
+  }
+}
