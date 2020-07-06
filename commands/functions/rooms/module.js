4aad193a964767d76ca405e2abdd0613904a02c6
Glitch (hello-express) <none>
2020-07-06 04:37:34 +0000

    🌂🐞 Checkpoint ./commands/functions/rooms/module.js:10282369/177

--- commands/functions/rooms/module.js
+++ commands/functions/rooms/module.js
@@ -11,9 +11,9 @@ module.exports = {
       channelLook: null, // Canal donde se fija para crear
       voiceCategory: null, // Categoría para crear los de voz
       textCategory: null, // Categoría para crear los de texto
-      activeRooms: {}, // Salas activas ex: {'DoTPr0': [audio,texto]}
+      createdRooms: {}, // Salas activas ex: {'DoTPr0': [audio,texto,date]}
       roomOwners: [], // Array de nombres de dueños
-      permission: {},
+      permission: {}, // Permisos de ingreso ex: {'DoTPr0': [Wephrix,Ace]}
     }).write()
   },
   
