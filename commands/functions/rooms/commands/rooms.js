a624db08f80a111cab01580e38b2f98e6ba31525
Glitch (hello-express) <none>
2020-07-06 06:13:02 +0000

    🏈💃 Checkpoint ./commands/functions/rooms/commands/rooms.js:10282369/294

--- commands/functions/rooms/commands/rooms.js
+++ commands/functions/rooms/commands/rooms.js
@@ -8,7 +8,12 @@ module.exports = {
   
   execute(Manager, msg, args) {
     if (msg.author.tag) {
+      /* 
       
+      Este es un comando admin, por ende voy a detectar cuando soy yo el que lo usa, para que nadie más pueda cambiar eso
+      Podría ponerle un generador de códigos, y que imprima el código por acá, tengo que ver todavía.
+      
+      */
     }
   }
 }
