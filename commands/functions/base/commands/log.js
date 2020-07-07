f96a9e79016cac733c88e88928f30ea332bf55c6
Glitch (private-rooms-discord) <none>
2020-07-07 21:49:24 +0000

    💶🏙 Checkpoint ./commands/functions/base/commands/log.js:10282369/3718 ./commands/functions/base/module.js:10282369/576

--- commands/functions/base/commands/log.js
+++ commands/functions/base/commands/log.js
@@ -1,12 +1,47 @@
+const { MessageEmbed } = require('discord.js')
+
 module.exports = {
   
-  trigger: "help",
+  trigger: "log",
   enabled: true,
   
   category: "Base",
-  description: "Send help to the chat",
+  description: "Send log to a chat",
   
   execute(Manager, msg, args) {
+    if (+msg.author.id === +Manager.botAuthor) {
+      /* 
+      Este es un comando admin, por ende voy a detectar cuando soy yo el que lo usa, para que nadie más pueda cambiar eso
+      Podría ponerle un generador de códigos, y que imprima el código por acá, tengo que ver todavía.
+      */
+      
+      let db = Manager.database;
+      
+      if(args[1]) { db.set('log',args[1]).write(); }
+      
+      const values = db.getState()
+      
+      const embed = new MessageEmbed().setColor('#dada3d')
+        .setTitle('Valores actuales')
+        .addFields(
+          {name: 'Channel Log Selected', value: values.log},
+        )
+      
+      msg.channel.send(embed)
+      
+    } else {
+      const embed = new MessageEmbed().setColor('#dada3d')
+        .setTitle(':no_entry_sign: Andateeeee')
+        .setDescription('Papaaaaaaa, no ves que este comando no podés usarlo?')
+      msg.channel.send(embed)
+    }
+  },
+  log(Manager,msg) {
+    let db = Manager.database;
     
+    
+    const embed = new MessageEmbed().setColor('#dada3d')
+      .setDescription(msg)
+    Manager.client.channels.fetch(log).then(channel => {channel.send(embed)})
   }
 }
--- commands/functions/base/module.js
+++ commands/functions/base/module.js
@@ -7,7 +7,9 @@ module.exports = {
   description: "Comandos Básicos",
   
   setup(Manager, db, googleKey) {
-    
+    db.defaults({
+      log: null, // Canal donde se manda el log
+    }).write()
   },
   
   execute(Manager, msg, args) {
