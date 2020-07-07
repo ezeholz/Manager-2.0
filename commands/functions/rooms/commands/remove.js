5a2d3fa34fca886fde6cb59d71e795a8368b2077
Glitch (hello-express) <none>
2020-07-07 03:24:44 +0000

    🎅💠 Checkpoint ./commands/functions/rooms/commands/remove.js:10282369/987 ./server.js:10282369/8 ./commands/index.js:10282369/4

--- commands/functions/rooms/commands/remove.js
+++ commands/functions/rooms/commands/remove.js
@@ -23,9 +23,8 @@ module.exports = {
             .setDescription('Para confirmar, por favor usar el comando .remove confirm')
           msg.channel.send(embed)
         } else {
-          let user = 
           Manager.client.channels.fetch(values.createdRooms[msg.author.id][0]).then(channel => {console.log(channel);channel.delete()})
-          values.createdRooms[msg.author.id] = undefined
+          db.get('createdRooms').set(msg.author.id,undefined).write()
           msg.channel.delete()
         }
       } else {
