82b0e3c660eeac4a4c2bd1c85ab33fb9f9bb904c
Glitch (private-rooms-discord) <none>
2020-07-08 04:13:39 +0000

    🐡🙈 Checkpoint ./commands/functions/streams/commands/stream.js:10282369/407

--- commands/functions/streams/commands/stream.js
+++ commands/functions/streams/commands/stream.js
@@ -51,12 +51,12 @@ module.exports = {
   async check(Manager){
     const today = Date.now()
     
-    let db = Manager.database;
+    let db = Manager.database.get('streams');
     
-    const entries = Object.entries(db.get('streams'))
-    const streamers = Object.keys(db.get('streams'))
+    const entries = Object.entries(db)
+    const streamers = Object.keys(db)
     
-    const online = entries.filter(e=>e[0]!==null)
+    const online = entries.filter(e=>e[1]!==null)
     
     if(streamers.length){
       let response = await fetch('https://id.twitch.tv/oauth2/token?client_id='+Manager.twitchClient+'&client_secret='+Manager.twitchToken+'&grant_type=client_credentials',{method:'POST'})
@@ -69,6 +69,12 @@ module.exports = {
         'Authorization': 'Bearer '+auth,
       }})
       json = await response.json()
+      
+      const offline = online.filter(e=>json.data.find(i=>i.user_name.toLowerCase()===e[0].toLowerCase())===undefined)
+      
+      offline.forEach(off => {
+        db.set(off[0],null)
+      })
       
       json.data.forEach(stream => {
         const found = entries.find(e => e[0].toLowerCase() === stream.user_name.toLowerCase())
