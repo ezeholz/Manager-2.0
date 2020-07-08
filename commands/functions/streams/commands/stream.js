7dc0d9da28d950c81ffdcd3bb6693314c71217a5
Glitch (private-rooms-discord) <none>
2020-07-08 03:25:38 +0000

    🐪📊 Checkpoint ./commands/functions/streams/commands/stream.js:10282369/1937 ./server.js:10282369/2

--- commands/functions/streams/commands/stream.js
+++ commands/functions/streams/commands/stream.js
@@ -9,7 +9,7 @@ module.exports = {
   category: "Streams",
   description: "Config streams",
   
-  async execute(Manager, msg, args) {
+  execute(Manager, msg, args) {
     const embed = new MessageEmbed().setColor('#dada3d')
     
     let db = Manager.database;
@@ -23,28 +23,7 @@ module.exports = {
       
       let db = Manager.database;
       
-      if(args[1]) {
-        let response = await fetch('https://id.twitch.tv/oauth2/token?client_id='+Manager.twitchClient+'&client_secret='+Manager.twitchToken+'&grant_type=client_credentials',{method:'POST'})
-        let json = await response.json()
-        
-        let auth = json.access_token
-        
-        // response = await fetch('https://api.twitch.tv/helix/users?login='+args[1],{headers:{
-        //   'Client-ID': Manager.twitchClient,
-        //   'Authorization': 'Bearer '+auth,
-        // }})
-        // json = await response.json()
-        
-        response = await fetch('https://api.twitch.tv/helix/streams?user_login='+args[1],{headers:{
-          'Client-ID': Manager.twitchClient,
-          'Authorization': 'Bearer '+auth,
-        }})
-        json = await response.json()
-        
-        console.log(json)
-        
-        //db.get('streams').set(json.data[0].id,null).write()
-      }
+      if(args[1]) db.get('streams').set(args[1],null).write()
       
       const values = db.getState()
       
@@ -69,11 +48,22 @@ module.exports = {
       msg.channel.send(embed)
     }
   },
-  check(Manager){
+  async check(Manager){
     const today = Date.now()
     
     let db = Manager.database;
     
     const values = Object.entries(db.get('streams'))
+    
+    let response = await fetch('https://id.twitch.tv/oauth2/token?client_id='+Manager.twitchClient+'&client_secret='+Manager.twitchToken+'&grant_type=client_credentials',{method:'POST'})
+    let json = await response.json()
+
+    let auth = json.access_token
+
+    response = await fetch('https://api.twitch.tv/helix/streams?user_login=',{headers:{
+      'Client-ID': Manager.twitchClient,
+      'Authorization': 'Bearer '+auth,
+    }})
+    json = await response.json()
   }
 }
